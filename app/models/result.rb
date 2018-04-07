9b8f4fc72a99d3fc2c6b9da9676b008e05eb8c64
beekermememe <bkeogh123@gmail.com>
2018-04-07 10:02:51 -0600

    Working on match wording - handling upcoming matches a little better

--- app/models/result.rb
+++ app/models/result.rb
@@ -14,15 +14,48 @@ class Result < ApplicationRecord
   def win
     win_count = 0
     loss_count = 0
-    sets.each do |set|
-      set_result = set.split("-")
-      if set_result[0].to_i < set_result[1].to_i
-        loss_count += 1
-      else
-        win_count += 1
+    if sets.count > 0
+      sets.each do |set|
+        set_result = set.split("-")
+        if set_result[0].to_i < set_result[1].to_i
+          loss_count += 1
+        else
+          win_count += 1
+        end
       end
+      win_count > loss_count
+    else
+      nil
     end
-    win_count > loss_count
+  end
+
+  def is_match_in_the_future
+    match = Match.find(match_id)
+    match.match_datetime &&  match.match_datetime > Time.now
+  end
+
+  def summary
+    win_count = 0
+    loss_count = 0
+    if sets.count > 0
+      sets.each do |set|
+        set_result = set.split("-")
+        if set_result[0].to_i < set_result[1].to_i
+          loss_count += 1
+        else
+          win_count += 1
+        end
+      end
+      if win_count > loss_count
+        "Win"
+      else
+        "Loss"
+      end
+    elsif is_match_in_the_future
+      "Upcoming"
+    else
+      "Please add result"
+    end
   end
 
   def show_sets
